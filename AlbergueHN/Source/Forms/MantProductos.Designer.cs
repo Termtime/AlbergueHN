19be81750fa2bead7425f791cf0c0ba4798ea058
Mario <mariofermejia99@gmail.com>
2020-11-08 00:48:54 -0600

    MantProductos MantPersonas

---
+++ AlbergueHN/Source/Forms/MantProductos.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AlbergueHN.Source.Forms
+{
+    partial class MantProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablaProductos = new System.Windows.Forms.DataGridView();
+            this.btnEliminarProducto = new System.Windows.Forms.Button();
+            this.btnCrearProducto = new System.Windows.Forms.Button();
+            this.btnModificarProducto = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablaProductos
+            // 
+            this.tablaProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaProductos.Location = new System.Drawing.Point(12, 101);
+            this.tablaProductos.Name = "tablaProductos";
+            this.tablaProductos.Size = new System.Drawing.Size(809, 600);
+            this.tablaProductos.TabIndex = 0;
+            // 
+            // btnEliminarProducto
+            // 
+            this.btnEliminarProducto.Location = new System.Drawing.Point(823, 604);
+            this.btnEliminarProducto.Name = "btnEliminarProducto";
+            this.btnEliminarProducto.Size = new System.Drawing.Size(75, 97);
+            this.btnEliminarProducto.TabIndex = 1;
+            this.btnEliminarProducto.Text = "Eliminar";
+            this.btnEliminarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnCrearProducto
+            // 
+            this.btnCrearProducto.Location = new System.Drawing.Point(827, 101);
+            this.btnCrearProducto.Name = "btnCrearProducto";
+            this.btnCrearProducto.Size = new System.Drawing.Size(75, 100);
+            this.btnCrearProducto.TabIndex = 2;
+            this.btnCrearProducto.Text = "Crear Producto";
+            this.btnCrearProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnModificarProducto
+            // 
+            this.btnModificarProducto.Location = new System.Drawing.Point(827, 207);
+            this.btnModificarProducto.Name = "btnModificarProducto";
+            this.btnModificarProducto.Size = new System.Drawing.Size(75, 100);
+            this.btnModificarProducto.TabIndex = 3;
+            this.btnModificarProducto.Text = "Modificar Producto";
+            this.btnModificarProducto.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 55);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Administrar Productos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Crear, Modificar y Eliminar productos";
+            // 
+            // MantProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 713);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnModificarProducto);
+            this.Controls.Add(this.btnCrearProducto);
+            this.Controls.Add(this.btnEliminarProducto);
+            this.Controls.Add(this.tablaProductos);
+            this.Name = "MantProductos";
+            this.Text = "Administrar Productos";
+            ((System.ComponentModel.ISupportInitialize)(this.tablaProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tablaProductos;
+        private System.Windows.Forms.Button btnEliminarProducto;
+        private System.Windows.Forms.Button btnCrearProducto;
+        private System.Windows.Forms.Button btnModificarProducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
