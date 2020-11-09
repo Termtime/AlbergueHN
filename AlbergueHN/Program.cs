8ee2c97c43a348310e406e142299c7b26221f6bf
Mario <mariofermejia99@gmail.com>
2020-11-08 19:54:38 -0600

    Agregado sistema de sesion

--- AlbergueHN/Program.cs
+++ AlbergueHN/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlbergueHN.Source.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,9 @@ namespace AlbergueHN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            LoginForm inicio = new LoginForm();
+            inicio.Show();
+            Application.Run();
         }
     }
 }
---
+++ AlbergueHN/Source/Objetos/UsuarioActual.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlbergueHN.Source.Objetos
+{
+    public static class UsuarioActual
+    {
+        public static string ID { get; set; } = null;
+        public static string usuario { get; set; } = null;
+    }
+}
